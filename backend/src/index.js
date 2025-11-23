9a6243c2e18593f81ecfe1d4ee81d38f1f97e52b
Lakshay <tyagilakshay47@gmail.com>
2025-11-23 21:38:36 +0530

    Fix axios export error

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,9 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
 app.use(cookieParser());
 app.use(
   cors({
