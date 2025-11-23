31d146d8bfd43ea96a37551e877cf7927bce1c8d
Lakshay <tyagilakshay47@gmail.com>
2025-11-23 16:44:08 +0530

    fix wildcard route for express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,42 +1,49 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import cors from 'cors';
+import {app, server} from './lib/socket.js';
+import path from 'path';
 
-import path from "path";
 
-import { connectDB } from "./lib/db.js";
 
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
 
+import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = process.env.PORT;
+
+import authRoutes from "./routes/auth.rote.js" 
+// Import the auth routes
+import messageRoutes from "./routes/message.route.js"
+ 
+import { connectDB } from './lib/db.js';
+
+
+
+
+
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
+app.use(cors({
+  origin: "http://localhost:5173",  // your frontend
+  credentials: true
+}));  
 
-app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+ // Middleware to parse JSON bodies
+app.use("/api/auth", authRoutes); 
+app.use("/api/message", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
-}
 
-server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
-  connectDB();
+}
+// Use the auth routes for /api/auth endpoint
+
+server.listen(PORT , () => {   
+    console.log("Server is running on port " + PORT);
+    connectDB();
 });
