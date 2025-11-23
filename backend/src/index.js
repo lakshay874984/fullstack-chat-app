75b4b765059ee8701a6ff2d2fa59501cf33bad24
Lakshay <tyagilakshay47@gmail.com>
2025-11-23 16:49:49 +0530

    fix wildcard route for express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,49 +1,42 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
-import {app, server} from './lib/socket.js';
-import path from 'path';
+import cors from "cors";
 
+import path from "path";
 
+import { connectDB } from "./lib/db.js";
 
+import authRoutes from "./routes/auth.rote.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-import dotenv from 'dotenv';
 dotenv.config();
 
-
-import authRoutes from "./routes/auth.rote.js" 
-// Import the auth routes
-import messageRoutes from "./routes/message.route.js"
- 
-import { connectDB } from './lib/db.js';
-
-
-
-
-
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
-app.use(cors({
-  origin: "http://localhost:5173",  // your frontend
-  credentials: true
-}));  
 
+app.use(express.json());
 app.use(cookieParser());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
- // Middleware to parse JSON bodies
-app.use("/api/auth", authRoutes); 
-app.use("/api/message", messageRoutes);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
+
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
-
 }
-// Use the auth routes for /api/auth endpoint
 
-server.listen(PORT , () => {   
-    console.log("Server is running on port " + PORT);
-    connectDB();
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
